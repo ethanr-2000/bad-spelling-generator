5d0c8ae1d0caf625fce6f9810d4421240e1692be
Ethan Roberts <robee031@gmail.com>
2022-12-31 15:34:32 +0000

    change to serverless

---
+++ lambda/index.py
@@ -0,0 +1,25 @@
+import re
+import json
+
+from helpers.spell_bad import spell_bad
+from helpers.util import load_pronunciation_dictionary, load_isle
+
+isleDict = load_isle()
+pd = load_pronunciation_dictionary()
+
+def handler(event, context):
+  request = json.loads(event['body'])
+
+  words_list = re.findall(r"[\w']+|[.,!?; ]", request['text'])
+  response = {"words": []}
+  for word in words_list:
+      if re.match(r"[.,!?; ]", word):
+          response['words'].append({'badlySpelled': [word]})
+          continue
+      bad_spell_response = spell_bad(word, isleDict, pd)
+      response['words'].append(bad_spell_response)
+
+  return {
+    'statusCode': 200,
+    'body': json.dumps(response)
+  }
