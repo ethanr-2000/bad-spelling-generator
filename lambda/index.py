a6598e4b6e095704ab358ebb080185b1496cbb77
ethanr-2000 <64696145+ethanr-2000@users.noreply.github.com>
2023-01-06 17:56:25 +0000

    Update index.py

--- lambda/index.py
+++ lambda/index.py
@@ -23,8 +23,7 @@ def handler(event, context):
     'statusCode': 200,
     'headers': {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': True,
-      'Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept"
+      'Access-Control-Allow-Credentials': True
     },
     'body': json.dumps(response)
   }
