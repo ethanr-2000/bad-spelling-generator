5d4209a4068484d5e99c17cb457eb59529775262
ethanr-2000 <64696145+ethanr-2000@users.noreply.github.com>
2023-01-06 17:24:33 +0000

    add cors headers

--- lambda/index.py
+++ lambda/index.py
@@ -21,5 +21,10 @@ def handler(event, context):
 
   return {
     'statusCode': 200,
+    'headers': {
+      "Access-Control-Allow-Headers" : "Content-Type",
+      "Access-Control-Allow-Origin": "*", 
+      "Access-Control-Allow-Methods": "GET"
+    }
     'body': json.dumps(response)
   }
